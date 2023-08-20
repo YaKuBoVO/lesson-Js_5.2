b79064067858b266828dce8fd77ee6b52c26bc5b
YaKuBoVO <oyakubov526@gmail.com>
2023-08-20 19:56:46 +0500

    Bugun tugatishing shart

--- script.js
+++ script.js
@@ -7,41 +7,40 @@ const unmute = document.querySelector('.unmute');
 const progressBar = document.querySelector('.progress_bar');
 const songWrapper = document.querySelector('.song-wrapper');
 const card = document.querySelector('.card');
-const active = document.querySelector('.active');
-
-card.addEventListener('click', () => {
-   card.classList.add('active');
-});
 
 
 
-play.addEventListener('click', function () {
-   audioPlayer.play();
-   this.style.display = 'none';
-   pause.style.display = 'block';
-});
-
-pause.addEventListener('click', function () {
-   audioPlayer.pause();
-   this.style.display = 'none';
-   play.style.display = 'block';
-});
-
-mute.addEventListener('click', function () {
-   audioPlayer.volume = 0;
-   this.style.display = 'none';
-   unmute.style.display = 'block';
-});
-unmute.addEventListener('click', function () {
-   audioPlayer.volume = 1;
-   this.style.display = 'none';
-   mute.style.display = 'block';
-});
-
-const size = 420;
-setInterval(function () {
-   const width = parseInt(
-      (audioPlayer.currentTime * size) / audioPlayer.duration
-   );
-   progressBar.style.width = width + 'px';
-}, 100);
+//card.addEventListener('click', () => {
+//   card.classList.add('active');
+//});
+   
+   play.addEventListener('click', function () {
+      audioPlayer.play();
+      this.style.display = 'none';
+      pause.style.display = 'block';
+   });
+   
+   pause.addEventListener('click', function () {
+      audioPlayer.pause();
+      this.style.display = 'none';
+      play.style.display = 'block';
+   });
+   
+   mute.addEventListener('click', function () {
+      audioPlayer.volume = 0;
+      this.style.display = 'none';
+      unmute.style.display = 'block';
+   });
+   unmute.addEventListener('click', function () {
+      audioPlayer.volume = 1;
+      this.style.display = 'none';
+      mute.style.display = 'block';
+   });
+   
+   const size = 420;
+   setInterval(function () {
+      const width = parseInt(
+         (audioPlayer.currentTime * size) / audioPlayer.duration
+      );
+      progressBar.style.width = width + 'px';
+   }, 100);
